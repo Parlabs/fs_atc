c8561c42c23bf72b2fdaa515725a9aaa815baa14
Parlabs <119888473+Parlabs@users.noreply.github.com>
2023-01-03 17:39:53 -0500

    7.1

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const tail_number = document.getElementById("tail_number").value;
-const airline = document.getElementById("airline").value;
-const callsign = document.getElementById("callsign").value;
-const flight_number = document.getElementById("flight_number").value;
-const altitude = document.getElementById("altitude").value;
-const speed = document.getElementById("speed").value;
-const ta_speed = document.getElementById("ta_speed").value;
-const vs = document.getElementById("vs").value;
-const route = document.getElementById("route").value;
-const dep_gate = document.getElementById("dep_gate").value;
-const arr_gate = document.getElementById("arr_gate").value;
+document.getElementsById("fly").addEventListener("click", function (e) {
+  const tail_number = document.getElementById("tail_number").value;
+  const airline = document.getElementById("airline").value;
+  const callsign = document.getElementById("callsign").value;
+  const flight_number = document.getElementById("flight_number").value;
+  const altitude = document.getElementById("altitude").value;
+  const speed = document.getElementById("speed").value;
+  const ta_speed = document.getElementById("ta_speed").value;
+  const vs = document.getElementById("vs").value;
+  const route = document.getElementById("route").value;
+  const dep_gate = document.getElementById("dep_gate").value;
+  const arr_gate = document.getElementById("arr_gate").value;
+});
