21a1d70de000894de881b72b9ec3ad6869bda6d5
Parlabs <119888473+Parlabs@users.noreply.github.com>
2023-01-07 17:01:09 -0500

    7.1.34

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-document.getElementsById("fly").addEventListener("click", function (e) {
+document.getElementsById("fly").addEventListener("click", function(e) {
+
+  e.preventDefault();
+
   const tail_number = document.getElementById("tail_number").value;
   const airline = document.getElementById("airline").value;
   const callsign = document.getElementById("callsign").value;
@@ -10,4 +13,22 @@ document.getElementsById("fly").addEventListener("click", function (e) {
   const route = document.getElementById("route").value;
   const dep_gate = document.getElementById("dep_gate").value;
   const arr_gate = document.getElementById("arr_gate").value;
+
+  document.getElementById("talk").addEventListener("click", function () {
+    if (document.getElementById("frequency").value === "atis") {
+      const dep_airport = route.split(' ').at(1);
+      const api_url = "https://avwx.rest/api/metar/" + dep_airport + "?airport=true&format=json";
+
+      async function getMetar() {
+        const response = await fetch(api_url);
+
+        const data = await response.json();
+
+        const msg = new SpeechSynthesisUtterance();
+        msg.text = data.resulsts;
+
+        
+      }
+    }
+  });
 });
